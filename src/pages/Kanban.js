40fb65a4c080d7a73a2b7041a37fddbd4c1d27a5
Michał Listek <michal.listex@gmail.com>
2022-08-05 16:40:12 +0200

    modified kanban apge and updated styling

--- src/pages/Kanban.js
+++ src/pages/Kanban.js
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import InputField from '../components/InputField';
+import TodoList from '../components/TodoList';
+
 export default function Kanban() {
-  return <h1>Kanban</h1>
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, {id: Date.now(), todo, status: false, user: 'Michał Listek'}]);
+      setTodo('');
+    }
+  };
+
+  return (
+    <div className='container'>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList />
+      {todos.map((t) => <li>{t.todo}</li>)}
+    </div>
+  );
 }

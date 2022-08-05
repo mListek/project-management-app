7569f3f6c539131b6529507b87655c406f2473b4
Michał Listek <michal.listex@gmail.com>
2022-08-05 20:21:40 +0200

    created TodoList

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,9 +1,12 @@
 import './TodoList.css';
 
-export default function TodoList() {
+export default function TodoList(props) {
   return (
-    <div className="todolist">
-      <h1>TodoList</h1>
+    <div className="todos">
+      {props.todos.map(todo => {
+        return <li key={todo.id}>{todo.todo}</li>
+      })}
     </div>
   );
 }
+// 44:20 video

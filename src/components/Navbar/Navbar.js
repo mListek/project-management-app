3078a5f4378a6b13b244901fa2cffe39a4892448
Michał Listek <michal.listex@gmail.com>
2022-08-05 20:16:48 +0200

    removed activeClassName

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,19 +7,19 @@ export default function Navbar() {
       <Link to="/" className="navbar__link--home">Home</Link>
       <ul>
         <li>
-          <NavLink to="kanban" activeClassName="active">Kanban</NavLink>
+          <NavLink to="kanban">Kanban</NavLink>
         </li>
         <li>
-          <NavLink to="gantt" activeClassName="active">Gantt</NavLink>
+          <NavLink to="gantt">Gantt</NavLink>
         </li>
         <li>
-          <NavLink to="files" activeClassName="active">Files</NavLink>
+          <NavLink to="files">Files</NavLink>
         </li>
         <li>
-          <NavLink to="settings" activeClassName="active">Settings</NavLink>
+          <NavLink to="settings">Settings</NavLink>
         </li>
         <li>
-          <NavLink to="login" activeClassName="active">Login</NavLink>
+          <NavLink to="login">Login</NavLink>
         </li>
       </ul>
     </nav>

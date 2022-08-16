443f7869119216bbfdccf24e3bae9249b5dfa6c0
Michał Listek <michal.listex@gmail.com>
2022-08-16 16:21:19 +0200

    created todo list

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,10 +3,52 @@ import SingleTodo from '../SingleTodo/SingleTodo';
 
 export default function TodoList(props) {
   return (
-    <div className="todos">
-      {props.todos.map(todo => {
-        return <SingleTodo key={todo.id} todo={todo} todos={props.todos} setTodos={props.setTodos} />
-      })}
+    <div className="todos__container">
+      <div className="todos__category neutral">
+        <span className="todos__heading">
+          To Do
+        </span>
+        {
+          props.todos.map((todo) => (
+            <SingleTodo
+              todo={todo}
+              todos={props.todos}
+              key={todo.id}
+              setTodos={props.setTodos}
+            />
+          ))
+        }
+      </div>
+      <div className="todos__category positive">
+        <span className="todos__heading">
+            In Progress
+        </span>
+        {
+          props.todos.map((todo) => (
+            <SingleTodo
+              todo={todo}
+              todos={props.todos}
+              key={todo.id}
+              setTodos={props.setTodos}
+            />
+          ))
+        }
+      </div>
+      <div className="todos__category negative">
+        <span className="todos__heading">
+            Done
+        </span>
+        {
+          props.todos.map((todo) => (
+            <SingleTodo
+              todo={todo}
+              todos={props.todos}
+              key={todo.id}
+              setTodos={props.setTodos}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
